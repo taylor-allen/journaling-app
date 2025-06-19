2f9321007a55cb1b478d3b945f7893b86b8099c5
taylorallen <twhitallen07@gmail.com>
2025-06-19 17:11:49 -0500

    updating

--- backend/app.py
+++ backend/app.py
@@ -11,6 +11,9 @@ MONGO_URI = os.environ.get("MONGO_URI")
 client = MongoClient(MONGO_URI)
 db = client["MindFrame-Journal"]
 
+@app.route("/")
+def index():
+    return jsonify(message="Welcome to the MindFrame Journal API!")
 @app.route("/api/hello")
 def hello():
     return "Hello from Flask!"
